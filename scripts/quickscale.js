49381157b79a8b61eec6ff40d5f6ffce0471f7fd
unsoluble <unsoluble@me.com>
2021-09-11 23:07:50 +0000

    base

---
+++ scripts/quickscale.js
@@ -0,0 +1,11 @@
+Hooks.on('init', () => {
+  /*
+  game.settings.register('quickscale', 'temp', {
+    name: 'Current Date',
+    scope: 'world',
+    config: false,
+    type: String,
+    default: '',
+  });
+  */
+});
