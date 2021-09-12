c2a63ebb4354fe70ba027fb3b9016cd0df5f2ca0
unsoluble <unsoluble@me.com>
2021-09-12 19:48:45 +0000

    hotkeys beginnings, plus slider stuff

--- scripts/quickscale.js
+++ scripts/quickscale.js
@@ -11,6 +11,65 @@ const QS_Animation_Path = 'modules/quickscale/assets/spinburst2.webm';
 const QS_Random_Floor = 0.8;
 const QS_Random_Ceiling = 1.2;
 
+Hooks.on('init', function () {
+  game.settings.register('quickscale', 'random-min', {
+    name: 'Random Min',
+    scope: 'world',
+    config: true,
+    type: Number,
+    default: 0.8,
+  });
+
+  game.settings.register('quickscale', 'random-max', {
+    name: 'Random Max',
+    scope: 'world',
+    config: true,
+    type: Number,
+    default: 1.2,
+  });
+
+  game.settings.register('quickscale', 'random-label', {
+    name: 'Randomization Range',
+    scope: 'world',
+    config: true,
+    type: Boolean,
+    default: true,
+  });
+
+  /*
+  
+  game.settings.register('quickscale', 'enlarge-key', {
+    scope: 'world',
+    config: false,
+    type: String,
+    default: '=',
+  });
+  
+  if (game.modules.get('lib-df-hotkeys')?.active) {
+    hotkeys.registerGroup({
+      name: 'quickscale.my-group',
+      label: 'My Awesome Group',
+      description: 'Optional description goes here',
+    });
+
+    hotkeys.registerShortcut({
+      name: 'quickscale.enlarge-key',
+      label: 'My Hotkey',
+      group: 'quickscale.my-group',
+      get: () => game.settings.get('quickscale', 'enlarge-key'),
+      set: async (value) => await game.settings.set('quickscale', 'enlarge-key', value),
+      default: () => {
+        return { key: hotkeys.keys.KeyQ, alt: false, ctrl: false, shift: false };
+      },
+      onKeyDown: (self) => {
+        console.log('You hit my custom hotkey!');
+      },
+    });
+  }
+  
+  */
+});
+
 Hooks.on('ready', () => {
   window.addEventListener('keypress', (e) => {
     // Don't trigger if we're in a text entry field.
@@ -31,6 +90,38 @@ Hooks.on('ready', () => {
       randomize();
     }
   });
+});
+
+Hooks.on('renderSettingsConfig', () => {
+  $('input[name="quickscale.random-min"]').parent().parent().css('display', 'none');
+  $('input[name="quickscale.random-max"]').parent().parent().css('display', 'none');
+  $('input[name="quickscale.random-label"]').css('display', 'none');
+
+  const insertionElement = $('input[name="quickscale.random-label"]').parent().next();
+
+  const injection = `<div id="quickscale-random-slider"></div>`;
+
+  // Only inject if it isn't already there.
+  if (!$('#quickscale-random-slider').length) {
+    insertionElement.after(injection);
+  }
+
+  const slider = document.getElementById('quickscale-random-slider');
+
+  noUiSlider.create(slider, {
+    start: [0.7, 1.2],
+    tooltips: [wNumb({ decimals: 1 }), wNumb({ decimals: 1 })],
+    step: 0.1,
+    margin: 0.1,
+    padding: 0.1,
+    connect: true,
+    range: {
+      min: 0.2,
+      max: 3.1,
+    },
+  });
+
+  mergeTooltips(slider, 5, ' - ');
 });
 
 async function updateSize(key) {
@@ -112,4 +203,77 @@ async function createAnimation(tokenID) {
   setTimeout(() => {
     token.removeChild(sprite);
   }, 1200);
+}
+
+function mergeTooltips(slider, threshold, separator) {
+  var textIsRtl = getComputedStyle(slider).direction === 'rtl';
+  var isRtl = slider.noUiSlider.options.direction === 'rtl';
+  var isVertical = slider.noUiSlider.options.orientation === 'vertical';
+  var tooltips = slider.noUiSlider.getTooltips();
+  var origins = slider.noUiSlider.getOrigins();
+
+  // Move tooltips into the origin element. The default stylesheet handles this.
+  tooltips.forEach(function (tooltip, index) {
+    if (tooltip) {
+      origins[index].appendChild(tooltip);
+    }
+  });
+
+  slider.noUiSlider.on('update', function (values, handle, unencoded, tap, positions) {
+    var pools = [[]];
+    var poolPositions = [[]];
+    var poolValues = [[]];
+    var atPool = 0;
+
+    // Assign the first tooltip to the first pool, if the tooltip is configured
+    if (tooltips[0]) {
+      pools[0][0] = 0;
+      poolPositions[0][0] = positions[0];
+      poolValues[0][0] = values[0];
+    }
+
+    for (var i = 1; i < positions.length; i++) {
+      if (!tooltips[i] || positions[i] - positions[i - 1] > threshold) {
+        atPool++;
+        pools[atPool] = [];
+        poolValues[atPool] = [];
+        poolPositions[atPool] = [];
+      }
+
+      if (tooltips[i]) {
+        pools[atPool].push(i);
+        poolValues[atPool].push(values[i]);
+        poolPositions[atPool].push(positions[i]);
+      }
+    }
+
+    pools.forEach(function (pool, poolIndex) {
+      var handlesInPool = pool.length;
+
+      for (var j = 0; j < handlesInPool; j++) {
+        var handleNumber = pool[j];
+
+        if (j === handlesInPool - 1) {
+          var offset = 0;
+
+          poolPositions[poolIndex].forEach(function (value) {
+            offset += 1000 - 10 * value;
+          });
+
+          var direction = isVertical ? 'bottom' : 'right';
+          var last = isRtl ? 0 : handlesInPool - 1;
+          var lastOffset = 1000 - 10 * poolPositions[poolIndex][last];
+          offset = (textIsRtl && !isVertical ? 100 : 0) + offset / handlesInPool - lastOffset;
+
+          // Center this tooltip over the affected handles
+          tooltips[handleNumber].innerHTML = poolValues[poolIndex].join(separator);
+          tooltips[handleNumber].style.display = 'block';
+          tooltips[handleNumber].style[direction] = offset + '%';
+        } else {
+          // Hide this tooltip
+          tooltips[handleNumber].style.display = 'none';
+        }
+      }
+    });
+  });
 }
