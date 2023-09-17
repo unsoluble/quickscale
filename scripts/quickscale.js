1818dd16d1acef3d02715d22c01b143b63aebc0e
John Weston <unsoluble@me.com>
2023-09-17 12:42:53 -0700

    take 2 on offset accounting

--- scripts/quickscale.js
+++ scripts/quickscale.js
@@ -264,9 +264,9 @@ Hooks.on('renderSettingsConfig', () => {
 // Gotta hook in here to handle the vertical offsets.
 Hooks.on('refreshToken', (token) => {
   if (!token.mesh) return;
-  const { offsetX, offsetY } = token.document.texture;
-  token.mesh.position.x += offsetX;
-  token.mesh.position.y += offsetY;
+  const offsetY = token.document.texture.offsetY;
+  const sizeOffset = token.h/2;
+  token.mesh.position.y = token.document.y + offsetY + sizeOffset;
 });
 
 function handleRandomScaleKey(currentToolLayer, key) {
